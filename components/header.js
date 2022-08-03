533370005c4863c53a61bee6ee0b0d0938ddf7c1
vplasencia <v.pcalana@gmail.com>
2022-08-02 22:35:59 -0400

    Add links header

--- components/header.js
+++ components/header.js
@@ -23,7 +23,7 @@ export default function Header() {
   ];
 
   return (
-    <div className="flex items-center justify-between py-8">
+    <div className="flex items-center justify-between py-8 px-6">
       <Link href="/">Logo</Link>
       <nav>
         <section className="MOBILE-MENU flex lg:hidden">
@@ -52,7 +52,7 @@ export default function Header() {
           <div className={isNavOpen ? styles.showMenuNav : styles.hideMenuNav}>
             <div
               className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
-              onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+              onClick={() => setIsNavOpen(false)}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -73,26 +73,43 @@ export default function Header() {
             <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
               {links.map((link, index) => {
                 return (
-                  <li
-                    className="border-b border-gray-400 my-8"
-                    key={index}
-                  >
-                    <a href={link.urlLink}>{link.textLink}</a>
+                  <li className={styles.navLink} key={index}>
+                    <Link href={link.urlLink}>{link.textLink}</Link>
                   </li>
                 );
               })}
+              <li>
+                <a
+                  href="/app"
+                  target="_blank"
+                  rel="noreferrer noopener nofollow"
+                  className="rounded-lg border-2 border-indigo-500 px-5 py-2 bg-indigo-500 hover:bg-transparent transition-colors"
+                >
+                  App
+                </a>
+              </li>
             </ul>
           </div>
         </section>
 
-        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex justify-center items-center">
           {links.map((link, index) => {
             return (
-              <li className="border-gray-400" key={index}>
-                <a href={link.urlLink}>{link.textLink}</a>
+              <li className={styles.navLink} key={index}>
+                <Link href={link.urlLink}>{link.textLink}</Link>
               </li>
             );
           })}
+          <li>
+            <a
+              href="/app"
+              target="_blank"
+              rel="noreferrer noopener nofollow"
+              className="rounded-lg font-medium border-2 border-indigo-500 px-5 py-2 bg-indigo-500 hover:shadow-lg hover:shadow-indigo-500 transition-shadow"
+            >
+              App
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
