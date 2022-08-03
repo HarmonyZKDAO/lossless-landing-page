2df775ade2f4ec5651a89cdbf9543ea17dfcfc1d
vplasencia <v.pcalana@gmail.com>
2022-08-02 22:52:40 -0400

    Add feather icons library

--- components/header.js
+++ components/header.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
 
+import { ExternalLink, Menu, X } from "react-feather";
+
 export default function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
@@ -31,22 +33,7 @@ export default function Header() {
             className="HAMBURGER-ICON space-y-2"
             onClick={() => setIsNavOpen((prev) => !prev)}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-menu"
-            >
-              <line x1="3" y1="12" x2="21" y2="12"></line>
-              <line x1="3" y1="6" x2="21" y2="6"></line>
-              <line x1="3" y1="18" x2="21" y2="18"></line>
-            </svg>
+            <Menu size={32} />
           </div>
 
           <div className={isNavOpen ? styles.showMenuNav : styles.hideMenuNav}>
@@ -54,21 +41,7 @@ export default function Header() {
               className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
               onClick={() => setIsNavOpen(false)}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-x"
-              >
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-              </svg>
+              <X size={32} />
             </div>
             <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
               {links.map((link, index) => {
@@ -83,9 +56,10 @@ export default function Header() {
                   href="/app"
                   target="_blank"
                   rel="noreferrer noopener nofollow"
-                  className="rounded-lg border-2 border-indigo-500 px-5 py-2 bg-indigo-500 hover:bg-transparent transition-colors"
+                  className="flex justify-center items-center space-x-1 rounded-lg tracking-wider font-semibold border-2 border-indigo-500 px-5 py-2 bg-indigo-500 hover:shadow-lg hover:shadow-indigo-500 transition-shadow"
                 >
-                  App
+                  <span>App</span>
+                  <ExternalLink size={20} />
                 </a>
               </li>
             </ul>
@@ -105,9 +79,10 @@ export default function Header() {
               href="/app"
               target="_blank"
               rel="noreferrer noopener nofollow"
-              className="rounded-lg font-medium border-2 border-indigo-500 px-5 py-2 bg-indigo-500 hover:shadow-lg hover:shadow-indigo-500 transition-shadow"
+              className="flex justify-center items-center space-x-1 rounded-lg tracking-wider font-semibold border-2 border-indigo-500 px-5 py-2 bg-indigo-500 hover:shadow-lg hover:shadow-indigo-500 transition-shadow"
             >
-              App
+              <span>App</span>
+              <ExternalLink size={20} />
             </a>
           </li>
         </ul>
