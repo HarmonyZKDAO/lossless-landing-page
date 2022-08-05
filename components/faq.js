cbdc27c9ae369dd464046f178b2745dd69784e21
vplasencia <v.pcalana@gmail.com>
2022-08-05 10:25:06 -0400

    Add faq accordion

--- components/faq.js
+++ components/faq.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 
 import { Twitter, GitHub, ChevronDown } from "react-feather";
 
+import styles from "../styles/Faq.module.css";
+
 export default function Faq() {
   const faqs = [
     { question: "Question 1", answer: "Answer 1" },
@@ -10,8 +12,34 @@ export default function Faq() {
   ];
 
   return (
-    <div>
-      <div className="text-2xl lg:text-3xl uppercase">FAQ</div>
+    <div className="bg-fuchsia-900 px-2">
+      <div className="mx-auto my-20 lg:w-6/12 rounded-md py-8">
+        <h1 className="text-2xl text-center text-white mb-10">
+          FAQ
+        </h1>
+
+        <div className="space-y-2">
+          <details className="duration-300">
+            <summary className="bg-inherit px-5 py-3 text-lg cursor-pointer rounded-lg border-2 flex justify-between">
+              <span className="select-none">Question 1</span>
+              <ChevronDown />
+            </summary>
+            <div className="px-5 py-3 text-sm font-light">
+              <p>Answer 1</p>
+            </div>
+          </details>
+
+          <details className="duration-300">
+            <summary className="bg-inherit px-5 py-3 text-lg cursor-pointer rounded-lg border-2 flex justify-between">
+              <span className="select-none">Question 2</span>
+              <ChevronDown />
+            </summary>
+            <div className="px-5 py-3 text-sm font-light">
+              <p>Answer 2</p>
+            </div>
+          </details>
+        </div>
+      </div>
     </div>
   );
 }
